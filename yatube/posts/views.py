91b3ab1d5f0d294edd9fd711b1644c6e67425da1
Leonid Turanov <rabcrin@gmail.com>
2022-06-04 20:46:51 +0300

    last changes

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Post
+#from django.http import HttpResponse
 
-def index(request):    
-    return HttpResponse('Главная страница')
+def index(request):
+    posts = Post.objects.order_by('-pub_date')[:10]
+    #template = 'posts/index.html'
+    #title = 'Главная страница'
+    context  = {
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context) 
 
-def group_posts(request, slug):    
-    return HttpResponse('Group')    
+def group_posts(request, slug):
+    template = 'posts/groups_list.html' 
+    title = 'Здесь будет информация о группах проекта Yatube'
+    context  = {'title': title}
+    return render(request, template, context) 
+
+
 
 # Create your views here.
